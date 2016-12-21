def2b1052713d0f3c19133ef755b94c148a64efc
dangerstephen <dangersteve1@gmail.com>
2016-12-21 11:54:50 -0800

    can now create new creatuers and show them by id

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -2,4 +2,25 @@ class CreaturesController < ApplicationController
     def index
       @creatures = Creature.all
     end
+
+    def new
+        @creature = Creature.new
+    end
+
+    def create
+        creature = Creature.create(creature_params)
+        redirect_to creature_path(creature)
+    end
+
+    def show
+        creature_id = params[:id]
+        @creature = Creature.find_by_id(creature_id)
+    end
+
+
+    private
+    def creature_params
+        creature_params = params.require(:creature).permit(:name, :description)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'creatures#index'
 
   get '/creatures' => 'creatures#index', as: 'creatures'
-
-
+  get '/creatures/new' => 'creatures#new', as: 'new_creature'
+  post '/creatures' => 'creatures#create'
+  get '/creatures/:id' => 'creatures#show', as: 'creature'
+  
 end
